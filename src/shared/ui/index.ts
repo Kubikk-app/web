3901c3376b385057a4086ecfb2687127659c9127
Vladimir <vovakay2003@gmail.com>
2023-08-15 21:54:57 +0300

    add a SliderRange.tsx component.

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,4 @@
 export {Input} from './Input/Input.tsx';
 export {Image} from './Image/Image.tsx';
 export {Avatar} from './Avatar/Avatar.tsx';
+export {SliderRange} from './SliderRange/SliderRange.tsx'
