c72181cfc0a48f3eae929cb7ec2b9b2e735308a8
saj1k2k <jonikbarezzi3@gmail.com>
2023-08-23 01:53:07 +0300

    fix: pull req fixes

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,42 +1,30 @@
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, HTMLInputTypeAttribute } from 'react';
 import styles from './Input.module.css';
 import cn from 'classnames';
 
-
-type HTMLInputProps = Omit<
-  InputHTMLAttributes<HTMLInputElement>,
-  'value'
->;
-
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value'>;
 
 interface InputProps extends HTMLInputProps {
   className?: string;
   value?: string | number;
   id?: string;
   error?: boolean;
-  type?: 'radio' | 'datetimeLocal' | 'password' | 'month' | 'number' | 'file' | 'email';
- }
-
+  // type?: 'radio' | 'datetimeLocal' | 'password' | 'month' | 'number' | 'file' | 'email';
+  type?: HTMLInputTypeAttribute;
+}
 
 export const Input = (props: InputProps) => {
-  const {
-    value,
-    type,
-    error,
-    id,
-    ...otherProps
-  } = props;
-
+  const { value, type, error, id, ...otherProps } = props;
 
   return (
-      <input
-        value={value}
-        className={cn(styles.Input, {
-          [styles.error]: error
-        })}
-        id={id}
-        type={type}
-        {...otherProps}
-      />
+    <input
+      value={value}
+      className={cn(styles.Input, {
+        [styles.error]: error,
+      })}
+      id={id}
+      type={type}
+      {...otherProps}
+    />
   );
 };
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,6 @@
-export {Input} from './Input/Input.tsx';
-export {Image} from './Image/Image.tsx';
-export {Avatar} from './Avatar/Avatar.tsx';
-export {Form} from './Form/Form.tsx';
-export {LabelInput} from './LabelInput/LabelInput.tsx';
-export {FormGroup} from './FormGroup/FormGroup.tsx';
+export { Input } from './Input/Input.tsx';
+export { Image } from './Image/Image.tsx';
+export { Avatar } from './Avatar/Avatar.tsx';
+export { Form } from './Form/Form.tsx';
+export { LabelInput } from './LabelInput/LabelInput.tsx';
+export { FormGroup } from './FormGroup/FormGroup.tsx';
