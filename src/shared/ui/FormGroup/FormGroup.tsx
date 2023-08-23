55583a87c60570ba3afba1692e44ed727c175ae2
saj1k2k <jonikbarezzi3@gmail.com>
2023-08-23 14:30:46 +0300

    fix: pull req fixes 2

--- src/shared/ui/FormGroup/FormGroup.tsx
+++ src/shared/ui/FormGroup/FormGroup.tsx
@@ -2,34 +2,21 @@ import { InputHTMLAttributes } from 'react';
 import styles from './FormGroup.module.css';
 import cn from 'classnames';
 
-
-type HTMLInputProps = Omit<
-  InputHTMLAttributes<HTMLDivElement>,
-  'value' | 'onChange'
->;
-
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLDivElement>, 'value' | 'onChange'>;
 
 interface FormGroupProps extends HTMLInputProps {
   className?: string;
-  row?: boolean;
-  vert?: boolean;
+  propsDirection?: 'row' | 'vert';
 }
 
-
 export const FormGroup = (props: FormGroupProps) => {
-  const {
-    children,
-    row,
-    vert,
-    ...otherProps
-  } = props;
-
+  const { children, propsDirection, ...otherProps } = props;
 
   return (
     <div
-      className={cn(styles.FormGroup,{
-        [styles.row]: row,
-        [styles.vert]: vert,
+      className={cn(styles.FormGroup, {
+        [styles.row]: propsDirection === 'row',
+        [styles.vert]: propsDirection === 'vert',
       })}
       {...otherProps}
     >
